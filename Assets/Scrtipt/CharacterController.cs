245afe80a709d39e8d576fc64412316d787fa674
Kirill <podgol04@gmail.com>
2024-12-15 19:44:33 +0300

    Исправление кода

--- Assets/Scrtipt/CharacterController.cs
+++ Assets/Scrtipt/CharacterController.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
-public class SimplePlayerController : MonoBehaviour
+public class PlayerCharacterController : MonoBehaviour
 {
     [Header("Movement Settings")]
     [SerializeField] private float walkSpeed = 3f;
     [SerializeField] private float runSpeed = 6f;
     [SerializeField] private float sprintSpeed = 9f;
     [SerializeField] private float rotationSpeed = 720f;
+    [SerializeField] private float mouseSensitivity = 1f;
 
     [Header("Gravity")]
     [SerializeField] private float gravity = -9.81f;
@@ -22,7 +23,9 @@ public class SimplePlayerController : MonoBehaviour
     private Vector2 inputDirection;
     private Vector3 velocity;
     private float currentSpeed;
+    private float yRotation;
 
+    // Animation IDs
     private int animIDSpeed;
     private int animIDGrounded;
     private int animIDJump;
@@ -35,6 +38,9 @@ public class SimplePlayerController : MonoBehaviour
         inputAsset = new InputAsset();
         AssignAnimationIDs();
         currentSpeed = walkSpeed;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private void OnEnable()
@@ -44,7 +50,15 @@ public class SimplePlayerController : MonoBehaviour
         inputAsset.Gameplay.Move.canceled += OnMoveCanceled;
         inputAsset.Gameplay.Sprint.started += OnSprintStarted;
         inputAsset.Gameplay.Sprint.canceled += OnSprintCanceled;
+        inputAsset.Gameplay.Look.performed += OnLook;
         inputAsset.Gameplay.Jump.started += OnJumpStarted;
+    }
+
+    private void OnLook(InputAction.CallbackContext obj)
+    {
+        Vector2 lookInput = obj.ReadValue<Vector2>();
+        yRotation += lookInput.x * mouseSensitivity;
+        transform.rotation = Quaternion.Euler(0, yRotation, 0);
     }
 
     private void OnDisable()
@@ -53,6 +67,7 @@ public class SimplePlayerController : MonoBehaviour
         inputAsset.Gameplay.Move.canceled -= OnMoveCanceled;
         inputAsset.Gameplay.Sprint.started -= OnSprintStarted;
         inputAsset.Gameplay.Sprint.canceled -= OnSprintCanceled;
+        inputAsset.Gameplay.Look.performed -= OnLook;
         inputAsset.Gameplay.Jump.started -= OnJumpStarted;
         inputAsset.Gameplay.Disable();
     }
@@ -76,12 +91,12 @@ public class SimplePlayerController : MonoBehaviour
 
     private void OnSprintStarted(InputAction.CallbackContext context)
     {
-        currentSpeed = sprintSpeed;  
+        currentSpeed = sprintSpeed;
     }
 
     private void OnSprintCanceled(InputAction.CallbackContext context)
     {
-        currentSpeed = runSpeed;  
+        currentSpeed = runSpeed;
     }
 
     private void OnJumpStarted(InputAction.CallbackContext context)
@@ -94,25 +109,28 @@ public class SimplePlayerController : MonoBehaviour
 
     private void HandleMovement()
     {
-        
         Vector3 moveDirection = new Vector3(inputDirection.x, 0, inputDirection.y).normalized;
 
-      
         if (moveDirection.sqrMagnitude > 0.01f)
         {
            
-            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-            controller.Move(moveDirection * currentSpeed * Time.deltaTime);
+            Vector3 forward = Camera.main.transform.forward;
+            forward.y = 0; 
+            Vector3 right = Camera.main.transform.right; 
+            Vector3 direction = forward * moveDirection.z + right * moveDirection.x;
+            if (direction.sqrMagnitude > 0.01f)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(direction); 
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime); 
+            }
+            controller.Move(direction * currentSpeed * Time.deltaTime);
         }
     }
-
     private void ApplyGravity()
     {
         if (controller.isGrounded && velocity.y < 0)
-        {
             velocity.y = -2f;
-        }
+
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
@@ -120,14 +138,15 @@ public class SimplePlayerController : MonoBehaviour
     private void UpdateAnimator()
     {
         if (animator == null) return;
+
         float speedPercent = currentSpeed / sprintSpeed;
         animator.SetFloat(animIDSpeed, speedPercent, 0.1f, Time.deltaTime);
-        animator.SetFloat(animIDMotionSpeed, speedPercent, 0.1f, Time.deltaTime);
         animator.SetBool(animIDGrounded, controller.isGrounded);
-        animator.SetBool(animIDJump, !controller.isGrounded && velocity.y > 0); 
+        animator.SetBool(animIDJump, !controller.isGrounded && velocity.y > 0);
         animator.SetBool(animIDFreeFall, !controller.isGrounded && velocity.y < 0);
-    }
 
+        animator.SetFloat(animIDMotionSpeed, speedPercent, 0.1f, Time.deltaTime);
+    }
 
     private void AssignAnimationIDs()
     {
